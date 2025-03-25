5121c532558d8ec727e4dcbb0fd650c21dc21a64
Rebel459 <131760345+Rebel459@users.noreply.github.com>
2025-03-25 19:57:41 +1100

    v1.0.7 new mixson

--- src/main/java/net/legacy/end_reborn/registry/ERTrimItemModels.java
+++ src/main/java/net/legacy/end_reborn/registry/ERTrimItemModels.java
@@ -5,184 +5,80 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.legacy.end_reborn.ERConstants;
 import net.minecraft.resources.ResourceLocation;
-import net.ramixin.mixson.Mixson;
-import net.ramixin.mixson.events.ModificationEvent;
-import org.jetbrains.annotations.NotNull;
+import net.ramixin.mixson.inline.Mixson;
 
 import java.util.List;
 
 public final class ERTrimItemModels {
-    private static final List<String> ARMORS = List.of("helmet", "chestplate", "leggings", "boots");
-    private static final List<String> ARMOR_MATERIALS = List.of("leather", "chainmail", "iron", "golden", "diamond", "netherite");
-    private static final List<String> ARMOR_MATERIALS_REMNANT = List.of("remnant");
-    private static final List<String> ARMOR_MATERIALS_FEATHERZEAL = List.of("featherzeal");
+    private static final List<String> ARMOR_PIECE_TYPES = List.of("helmet", "chestplate", "leggings", "boots");
 
-    @SuppressWarnings("UnnecessaryReturnStatement")
-    private ERTrimItemModels() {
-        return;
+    private static final List<ResourceLocation> ARMOR_MATERIALS = List.of(
+            ResourceLocation.withDefaultNamespace("leather"),
+            ResourceLocation.withDefaultNamespace("chainmail"),
+            ResourceLocation.withDefaultNamespace("iron"),
+            ResourceLocation.withDefaultNamespace("golden"),
+            ResourceLocation.withDefaultNamespace("diamond"),
+            ResourceLocation.withDefaultNamespace("netherite")
+    );
+
+
+    public static void init() {}
+
+    static {
+        ARMOR_PIECE_TYPES.forEach(armorType -> ARMOR_MATERIALS.forEach(material -> registerItemModelModification(armorType, material)));
+        registerItemModelModification("helmet", ResourceLocation.withDefaultNamespace("turtle"));
+
+        registerAtlasModification("armor_trims");
+        registerAtlasModification("blocks");
     }
 
-    public static void init() {
-        /*
-         * Add trim materials to each armor.
-         */
-        ARMORS.forEach(armor -> ARMOR_MATERIALS.forEach(armorMaterial -> registerAddTrimsToArmor(armor, armorMaterial)));
-        ARMORS.forEach(armor -> ARMOR_MATERIALS_REMNANT.forEach(armorMaterial -> registerAddTrimsToArmorRemnant(armor, armorMaterial)));
-        ARMORS.forEach(armor -> ARMOR_MATERIALS_FEATHERZEAL.forEach(armorMaterial -> registerAddTrimsToArmorFeatherzeal(armor, armorMaterial)));
+    private static void registerItemModelModification(String armorPieceType, ResourceLocation armorMaterial) {
+        Mixson.registerEvent(
+                1,
+                ResourceLocation.fromNamespaceAndPath(armorMaterial.getNamespace(), "items/" + armorMaterial.getPath() + "_" + armorPieceType).toString(),
+                ERConstants.id("add_trims_to_" + armorMaterial.getPath() + "_" + armorPieceType).toString(),
+                (context) -> {
+                    JsonObject rootJson = context.getFile().getAsJsonObject();
+                    JsonObject modelJson = rootJson.getAsJsonObject("model");
+                    JsonArray casesArray = modelJson.getAsJsonArray("cases");
+                    JsonObject baseCase = casesArray.get(0).getAsJsonObject();
 
-        // (dare to be different)
-        registerAddTrimsToArmor("helmet", "turtle");
+                    ERTrimMaterials.TRIM_MATERIALS.forEach(trimMaterial -> {
+                        JsonObject newCase = baseCase.deepCopy();
 
-        /*
-         * Add trim materials to vanilla atlases.
-         */
-        registerAddTrimsToAtlas("armor_trims");
-        registerAddTrimsToAtlas("blocks");
-    }
+                        newCase.addProperty("when", trimMaterial.location().toString());
+                        newCase.getAsJsonObject("model").addProperty("model", ERConstants.id("item/" + armorMaterial.getPath() + "_" + armorPieceType + "_" + trimMaterial.location().getPath() + "_trim").toString());
 
-    private static void registerAddTrimsToArmor(String armor, String armorMaterial) {
-        Mixson.registerModificationEvent(
-                ResourceLocation.withDefaultNamespace("items/" + armorMaterial + "_" + armor),
-                ResourceLocation.fromNamespaceAndPath(ERConstants.MOD_ID, "add_trims_to_" + armorMaterial + "_" + armor),
-                new ModificationEvent() {
-                    @Override
-                    public @NotNull JsonElement run(JsonElement elem) {
-                        JsonObject root = elem.getAsJsonObject();
-                        JsonObject model = root.getAsJsonObject("model");
-                        JsonArray cases = model.getAsJsonArray("cases");
-                        JsonObject case0 = cases.get(0).getAsJsonObject();
-
-                        ERTrimMaterials.TRIM_MATERIALS.forEach(trim -> {
-                            JsonObject newCase = case0.deepCopy();
-
-                            newCase.addProperty("when", trimMaterialId(trim).toString());
-                            newCase.getAsJsonObject("model")
-                                    .addProperty("model", itemModelId(armor, armorMaterial, trim).toString());
-
-                            cases.add(newCase);
-                        });
-
-                        return elem;
-                    }
-
-                    @Override
-                    public int ordinal() {
-                        return 0;
-                    }
+                        casesArray.add(newCase);
+                    });
                 }
         );
     }
 
-    private static void registerAddTrimsToArmorRemnant(String armor, String armorMaterial) {
-        Mixson.registerModificationEvent(
-                ResourceLocation.fromNamespaceAndPath(ERConstants.MOD_ID, "items/" + armorMaterial + "_" + armor),
-                ResourceLocation.fromNamespaceAndPath(ERConstants.MOD_ID, "add_trims_to_" + armorMaterial + "_" + armor),
-                new ModificationEvent() {
-                    @Override
-                    public @NotNull JsonElement run(JsonElement elem) {
-                        JsonObject root = elem.getAsJsonObject();
-                        JsonObject model = root.getAsJsonObject("model");
-                        JsonArray cases = model.getAsJsonArray("cases");
-                        JsonObject case0 = cases.get(0).getAsJsonObject();
+    private static void registerAtlasModification(String atlasName) {
+        Mixson.registerEvent(
+                1,
+                ResourceLocation.withDefaultNamespace("atlases/" + atlasName).toString(),
+                ERConstants.id("add_trims_to_" + atlasName + "_atlas").toString(),
+                (context) -> {
+                    JsonObject rootJson = context.getFile().getAsJsonObject();
+                    JsonArray sourcesArray = rootJson.getAsJsonArray("sources");
 
-                        ERTrimMaterials.TRIM_MATERIALS.forEach(trim -> {
-                            JsonObject newCase = case0.deepCopy();
+                    for (JsonElement sourceElement : sourcesArray) {
+                        JsonObject sourceObject = sourceElement.getAsJsonObject();
 
-                            newCase.addProperty("when", trimMaterialId(trim).toString());
-                            newCase.getAsJsonObject("model")
-                                    .addProperty("model", itemModelId(armor, armorMaterial, trim).toString());
+                        if ("paletted_permutations".equals(sourceObject.get("type").getAsString())) {
+                            JsonObject permutationsJson = sourceObject.getAsJsonObject("permutations");
 
-                            cases.add(newCase);
-                        });
+                            ERTrimMaterials.TRIM_MATERIALS.forEach(trimMaterial -> {
+                                String trimName = trimMaterial.location().getPath();
+                                permutationsJson.addProperty(trimName, ERConstants.id("trims/color_palettes/" + trimName).toString());
+                            });
 
-                        return elem;
-                    }
-
-                    @Override
-                    public int ordinal() {
-                        return 0;
-                    }
-                }
-        );
-    }
-
-    private static void registerAddTrimsToArmorFeatherzeal(String armor, String armorMaterial) {
-        Mixson.registerModificationEvent(
-                ResourceLocation.fromNamespaceAndPath(ERConstants.MOD_ID, "items/" + armorMaterial + "_" + armor),
-                ResourceLocation.fromNamespaceAndPath(ERConstants.MOD_ID, "add_trims_to_" + armorMaterial + "_" + armor),
-                new ModificationEvent() {
-                    @Override
-                    public @NotNull JsonElement run(JsonElement elem) {
-                        JsonObject root = elem.getAsJsonObject();
-                        JsonObject model = root.getAsJsonObject("model");
-                        JsonArray cases = model.getAsJsonArray("cases");
-                        JsonObject case0 = cases.get(0).getAsJsonObject();
-
-                        ERTrimMaterials.TRIM_MATERIALS.forEach(trim -> {
-                            JsonObject newCase = case0.deepCopy();
-
-                            newCase.addProperty("when", trimMaterialId(trim).toString());
-                            newCase.getAsJsonObject("model")
-                                    .addProperty("model", itemModelId(armor, armorMaterial, trim).toString());
-
-                            cases.add(newCase);
-                        });
-
-                        return elem;
-                    }
-
-                    @Override
-                    public int ordinal() {
-                        return 0;
-                    }
-                }
-        );
-    }
-
-    private static void registerAddTrimsToAtlas(String name) {
-        Mixson.registerModificationEvent(
-                ResourceLocation.withDefaultNamespace("atlases/" + name),
-                ResourceLocation.fromNamespaceAndPath(ERConstants.MOD_ID, "add_trims_to_" + name + "_atlas"),
-                new ModificationEvent() {
-                    @Override
-                    public @NotNull JsonElement run(JsonElement elem) {
-                        JsonObject root = elem.getAsJsonObject();
-                        JsonArray sources = root.getAsJsonArray("sources");
-
-                        for (int i = 0; i < sources.size(); ++i) {
-                            JsonObject source = sources.get(i).getAsJsonObject();
-
-                            if ("paletted_permutations".equals(source.getAsJsonPrimitive("type").getAsString())) {
-                                JsonObject permutations = source.getAsJsonObject("permutations");
-
-                                ERTrimMaterials.TRIM_MATERIALS.forEach(trim ->
-                                        permutations.addProperty(trim, paletteId(trim).toString())
-                                );
-
-                                break;
-                            }
+                            break;
                         }
-
-                        return elem;
-                    }
-
-                    @Override
-                    public int ordinal() {
-                        return 0;
                     }
                 }
         );
-    }
-
-    private static ResourceLocation trimMaterialId(String trim) {
-        return ResourceLocation.fromNamespaceAndPath(ERConstants.MOD_ID, trim);
-    }
-
-    private static ResourceLocation itemModelId(String armor, String armorMaterial, String trim) {
-        return ResourceLocation.fromNamespaceAndPath(ERConstants.MOD_ID, "item/" + armorMaterial + "_" + armor + "_" + trim + "_trim");
-    }
-
-    private static ResourceLocation paletteId(String trim) {
-        return ResourceLocation.fromNamespaceAndPath(ERConstants.MOD_ID, "trims/color_palettes/" + trim);
     }
 }
--- src/main/java/net/legacy/end_reborn/registry/ERTrimMaterials.java
+++ src/main/java/net/legacy/end_reborn/registry/ERTrimMaterials.java
@@ -1,35 +1,48 @@
 package net.legacy.end_reborn.registry;
 
 import net.legacy.end_reborn.ERConstants;
+import net.minecraft.Util;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.equipment.trim.TrimMaterial;
 import net.minecraft.world.item.equipment.trim.TrimMaterials;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class ERTrimMaterials {
-    public static final List<String> TRIM_MATERIALS = List.of("remnant_ingot", "remnant_ingot_darker", "featherzeal_ingot", "featherzeal_ingot_darker", "crystalline_shard");
+    public static final List<ResourceKey<TrimMaterial>> TRIM_MATERIALS = new ArrayList<>();
 
-    public static final ResourceKey<TrimMaterial> REMNANT = createResourceKey("remnant_ingot");
-    public static final ResourceKey<TrimMaterial> REMNANT_DARKER = createResourceKey("remnant_ingot_darker");
-    public static final ResourceKey<TrimMaterial> FEATHERZEAL = createResourceKey("featherzeal_ingot");
-    public static final ResourceKey<TrimMaterial> FEATHERZEAL_DARKER = createResourceKey("featherzeal_ingot_darker");
-    public static final ResourceKey<TrimMaterial> CRYSTALLINE = createResourceKey("crystalline_shard");
+    public static final ResourceKey<TrimMaterial> REMNANT = register("remnant_ingot");
+    public static final ResourceKey<TrimMaterial> REMNANT_DARKER = register("remnant_ingot_darker");
+    public static final ResourceKey<TrimMaterial> FEATHERZEAL = register("featherzeal_ingot");
+    public static final ResourceKey<TrimMaterial> FEATHERZEAL_DARKER = register("featherzeal_ingot_darker");
+    public static final ResourceKey<TrimMaterial> CRYSTALLINE = register("crystalline_shard");
 
-    public static void bootstrap(BootstrapContext<TrimMaterial> bootstrapContext) {
-        TrimMaterials.register(bootstrapContext, REMNANT, ERItems.REMNANT_INGOT, Style.EMPTY.withColor(5592405), Map.of(EREquipmentAssets.REMNANT, "remnant_ingot_darker"));
-        TrimMaterials.register(bootstrapContext, REMNANT_DARKER, ERItems.REMNANT_INGOT, Style.EMPTY.withColor(5592405));
-        TrimMaterials.register(bootstrapContext, FEATHERZEAL, ERItems.FEATHERZEAL_INGOT, Style.EMPTY.withColor(5090680), Map.of(EREquipmentAssets.REMNANT, "featherzeal_ingot_darker"));
-        TrimMaterials.register(bootstrapContext, FEATHERZEAL_DARKER, ERItems.FEATHERZEAL_INGOT, Style.EMPTY.withColor(5090680));
-        TrimMaterials.register(bootstrapContext, CRYSTALLINE, ERItems.CRYSTALLINE_SHARD, Style.EMPTY.withColor(16777215));
+    public static void bootstrap(BootstrapContext<TrimMaterial> context) {
+        register(context, REMNANT, ERItems.REMNANT_INGOT, Style.EMPTY.withColor(5592405));
+        register(context, REMNANT_DARKER, ERItems.REMNANT_INGOT, Style.EMPTY.withColor(5592405));
+        register(context, FEATHERZEAL, ERItems.FEATHERZEAL_INGOT, Style.EMPTY.withColor(5090680));
+        register(context, FEATHERZEAL_DARKER, ERItems.FEATHERZEAL_INGOT, Style.EMPTY.withColor(5090680));
+        register(context, CRYSTALLINE, ERItems.CRYSTALLINE_SHARD, Style.EMPTY.withColor(16777215));
     }
 
-    private static ResourceKey<TrimMaterial> createResourceKey(String string) {
-        return ResourceKey.create(Registries.TRIM_MATERIAL, ResourceLocation.fromNamespaceAndPath(ERConstants.MOD_ID, string));
+    private static void register(BootstrapContext<TrimMaterial> context, ResourceKey<TrimMaterial> key, Item item, Style style) {
+        ResourceLocation location = key.location();
+        TrimMaterial material = TrimMaterial.create(location.getPath(), item, Component.translatable(Util.makeDescriptionId("trim_material", location)).withStyle(style), Map.of());
+        context.register(key, material);
+    }
+
+    private static ResourceKey<TrimMaterial> register(String name) {
+        ResourceKey<TrimMaterial> key = ResourceKey.create(Registries.TRIM_MATERIAL, ERConstants.id(name));
+        TRIM_MATERIALS.add(key);
+        return key;
     }
 }
